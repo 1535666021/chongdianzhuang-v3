04e7d7678082c9c5813331a4d5e98e4b983c9782
1535666021 <50433318+1535666021@users.noreply.github.com>
2026-07-24 15:21:46 +0800

    feat(batch2): update routes with OrderForm + BatchParser (6/8)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import { RoutePath } from './route'
 import App from '@/App'
 import OrderList from '@/features/order/pages/OrderList'
 import OrderDetail from '@/features/order/pages/OrderDetail'
+import OrderForm from '@/features/order/pages/OrderForm'
+import BatchParser from '@/features/batchParser/pages/BatchParser'
 
 // 懒加载页面（后续实现）
 const MaterialsPage = () => <div>材料占位</div>
@@ -16,6 +18,9 @@ export default function AppRoutes() {
         <Route path="/" element={<App />}>
           <Route index element={<OrderList />} />
           <Route path="orders/:id" element={<OrderDetail />} />
+          <Route path="order/new" element={<OrderForm />} />
+          <Route path="order/edit/:id" element={<OrderForm />} />
+          <Route path="batch-parser" element={<BatchParser />} />
           <Route path={RoutePath.MATERIALS} element={<MaterialsPage />} />
           <Route path={RoutePath.STATS} element={<StatsPage />} />
           <Route path={RoutePath.SETTINGS} element={<SettingsPage />} />
