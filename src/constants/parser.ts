d5b55fc328b427edad171469c4c9ad946720023b
1535666021 <50433318+1535666021@users.noreply.github.com>
2026-07-24 14:00:27 +0800

    feat: add src/constants/parser.ts (file 23/44)

--- src/constants/parser.ts
+++ src/constants/parser.ts
@@ -1,14 +1,9 @@
 export const ORDER_TEXT_PATTERNS = {
-  // 匹配订单文本中的关键信息
-  customerName: /客户姓名[：:]\s*(.+?)(?:
-|$)/,
+  customerName: /客户姓名[：:]\s*(.+?)(?:\n|$)/,
   phone: /电话[：:]\s*(\d{11})/,
-  address: /地址[：:]\s*(.+?)(?:
-|$)/,
-  platform: /平台[：:]\s*(.+?)(?:
-|$)/,
-  appointment: /预约[：:]\s*(\d{4}-\d{2}-\d{2})\s*(\d{2}:\d{2})?/,
+  address: /地址[：:]\s*(.+?)(?:\n|$)/,
+  platform: /平台[：:]\s*(.+?)(?:\n|$)/,
+  appointment: /预约[：:]\s*(\d{4}-\d{2}-\d{2})\s*(\d{2}:\d{2})/,
 }
 
-export const LINE_SEPARATOR = '---'
-export const FIELD_SEPARATOR = '|'
+export const DEFAULT_SEPARATOR = '---'
