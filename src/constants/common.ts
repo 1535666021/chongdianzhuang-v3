c2c5600c9aee068a9dc5ac5df9318d5e2047be68
1535666021 <50433318+1535666021@users.noreply.github.com>
2026-07-24 13:57:51 +0800

    feat: add src/constants/common.ts (file 20/44)

---
+++ src/constants/common.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = '充电桩订单助手'
+export const APP_VERSION = '3.0.0'
+export const STORAGE_KEY_PREFIX = 'cdz_v3_'
+
+export const DEFAULT_ENGINEER = {
+  name: '谢责强',
+  phone: '15395147568',
+}
